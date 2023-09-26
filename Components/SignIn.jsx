f00995c8960c3a21f112ee6168551b17aa53bb59
am-here <harshbaid.ind@gmail.com>
2023-09-27 03:26:48 +0530

    deployment issue 1 solved try 6

--- Components/SignIn.jsx
+++ Components/SignIn.jsx
@@ -1,6 +1,5 @@
 "use client"
 import { useSession, signIn, signOut } from "next-auth/react"
-import { WhiteButton } from "./Buttons";
 
 function SignInButton() {
     const { status } = useSession();
@@ -14,9 +13,13 @@ function SignInButton() {
         <>
             {
                 status === "authenticated" ? (
-                    <WhiteButton onClick={signOutHandler} text="Sign Out" />
+                    <button type="button" onClick={signOutHandler} className="text-black bg-white hover:bg-white/90 focus:ring-4 focus:outline-none focus:ring-white/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
+                        Sign Out
+                    </button>
                 ) : (
-                    <WhiteButton onClick={signInHandler} text="Sign In" />
+                    <button type="button" onClick={signInHandler} className="text-black bg-white hover:bg-white/90 focus:ring-4 focus:outline-none focus:ring-white/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center ">
+                        Sign In
+                    </button>
                 )
             }
         </>
